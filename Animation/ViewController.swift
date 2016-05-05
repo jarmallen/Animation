3078a8df5832376669d73cf1d20b795efd29fe73
Jared Allen <jarmallen@gmail.com>
2016-05-05 10:30:26 -0700

    Commit again

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -10,16 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rangerImage: UIImageView!
+    
+    var counter = 1
+    var timer = NSTimer()
+    var isAnimating = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func doAnimation() {
+        if counter == 22 {
+            counter = 1
+            
+        } else {
+            counter++
+            
+        }
+        
+        rangerImage.image = UIImage(named: "yellow-ranger-\(counter).png")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func animateButton(sender: AnyObject) {
+       
+        if isAnimating == false {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            isAnimating = true
+
+        } else {
+            timer.invalidate()
+            isAnimating = false
+            
+        }
+       
     }
-
-
+    
+     /*
+    override func viewDidLayoutSubviews() {
+        // rangerImage.center = CGPointMake(rangerImage.center.x - 400, rangerImage.center.y)
+        // rangerImage.alpha = 0
+        rangerImage.frame = CGRectMake(5, 20, 0, 0)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1, animations: { () -> Void in
+            // self.rangerImage.center = CGPointMake(self.rangerImage.center.x + 400, self.rangerImage.center.y)
+            // self.rangerImage.alpha = 1
+            self.rangerImage.frame = CGRectMake(5, 20, 400, 200)
+        })
+    }
+    */
 }
 
